ac17412eb325122e0dfd0ddbfb79699b7894cb04
sanaeas <sanaassim1@gmail.com>
2023-03-20 19:21:18 +0000

    Remove pdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,34 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from '../assets/img/logo.png';
-import githubLogo from '../assets/img/github.svg';
-import pdfLogo from '../assets/img/pdf.png';
-import resume from '../assets/resume.pdf';
+import logo from "../assets/img/logo.png";
+import githubLogo from "../assets/img/github.svg";
+import pdfLogo from "../assets/img/pdf.png";
 
 function Footer() {
     return (
-      <footer className="footer">
-        <Container>
-          <Row className="align-items-center">
-            <Col size={12} sm={6}>
-              <img src={logo} alt="Logo" />
-            </Col>
-            <Col size={12} sm={6} className="text-center text-sm-end">
-              <div className="social-icon">
-                <a target="_blank" href="https://github.com/sanaeas"><img src={githubLogo} alt="Icon" /></a>
-                <a target="_blank" href={resume}><img src={pdfLogo} alt="Icon" /></a>
-              </div>
-              <p>Copyright 2022. All Rights Reserved</p>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    )
-  }
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} sm={6}>
+                        <img src={logo} alt="Logo" />
+                    </Col>
+                    <Col size={12} sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/sanaeas">
+                                <img src={githubLogo} alt="Icon" />
+                            </a>
+                            <a href="/">
+                                <img src={pdfLogo} alt="Icon" />
+                            </a>
+                        </div>
+                        <p>Copyright 2022. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
 
-  export default Footer;
+export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/img/logo.png';
-import githubLogo from '../assets/img/github.svg';
-import pdfLogo from '../assets/img/pdf.png';
-import resume from '../assets/resume.pdf';
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/img/logo.png";
+import githubLogo from "../assets/img/github.svg";
+import pdfLogo from "../assets/img/pdf.png";
 import { BrowserRouter as Router } from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
 
 function NavBarComp() {
-    const [activeLink, setActiveLink] = useState('home');
+    const [activeLink, setActiveLink] = useState("home");
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
@@ -20,49 +19,83 @@ function NavBarComp() {
             } else {
                 setScrolled(false);
             }
-        }
+        };
 
-        window.addEventListener('scroll', onScroll);
+        window.addEventListener("scroll", onScroll);
 
-        return () => window.removeEventListener('scroll', onScroll);
-    }, [])
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
-    }
+    };
 
-  return (
-    <Router>
-      <Navbar className={scrolled ? "scrolled" : ""} expand="md">
-        <Container>
-          <Navbar.Brand href="/">
-              <img className='nav__logo' src={logo} alt='Logo' />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-              <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} >Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} >Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                  <a target="_blank" href="https://github.com/sanaeas"><img src={githubLogo} alt='' /></a>
-                  <a target="_blank" href={resume}><img src={pdfLogo} alt='' /></a>
-              </div>
-              <HashLink to="#connect">
-              <button className="vvd">
-                  <span>Let's Connect</span>
-              </button>
-              </HashLink>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Router>
-  )
+    return (
+        <Router>
+            <Navbar className={scrolled ? "scrolled" : ""} expand="md">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img className="nav__logo" src={logo} alt="Logo" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link
+                                href="#home"
+                                className={
+                                    activeLink === "home"
+                                        ? "active navbar-link"
+                                        : "navbar-link"
+                                }
+                                onClick={() => onUpdateActiveLink("home")}>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link
+                                href="#skills"
+                                className={
+                                    activeLink === "skills"
+                                        ? "active navbar-link"
+                                        : "navbar-link"
+                                }
+                                onClick={() => onUpdateActiveLink("skills")}>
+                                Skills
+                            </Nav.Link>
+                            <Nav.Link
+                                href="#projects"
+                                className={
+                                    activeLink === "projects"
+                                        ? "active navbar-link"
+                                        : "navbar-link"
+                                }
+                                onClick={() => onUpdateActiveLink("projects")}>
+                                Projects
+                            </Nav.Link>
+                        </Nav>
+                        <span className="navbar-text">
+                            <div className="social-icon">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href="https://github.com/sanaeas">
+                                    <img src={githubLogo} alt="" />
+                                </a>
+                                <a href="/">
+                                    <img src={pdfLogo} alt="" />
+                                </a>
+                            </div>
+                            <HashLink to="#connect">
+                                <button className="vvd">
+                                    <span>Let's Connect</span>
+                                </button>
+                            </HashLink>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Router>
+    );
 }
 
-export default NavBarComp
+export default NavBarComp;
